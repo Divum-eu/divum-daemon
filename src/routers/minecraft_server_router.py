18cf2f6deb283c25898f3d906576c20e8fc5528b
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-04-15 18:50:43 +0300

    fix: solve api differences from merge

--- src/routers/minecraft_server_router.py
+++ src/routers/minecraft_server_router.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from dependencies.services import get_docker_server_manager
+from schemas.minecraft_server_itzg_config import MinecraftServerITZGConfig
 from services.server_manager import ServerManager
 
 DockerServerManagerDependency = Annotated[
@@ -17,18 +18,18 @@ minecraft_server_router = APIRouter(
 
 @minecraft_server_router.post("/")
 async def create_minecraft_server(
-    name: str, server_manager: DockerServerManagerDependency
+    request: MinecraftServerITZGConfig, server_manager: DockerServerManagerDependency
 ):
-    server_manager.create_server(name)
+    server_manager.create(request)
 
 
 @minecraft_server_router.post("/{id}/start")
 async def start_minecraft_server(
     id: str, server_manager: DockerServerManagerDependency
 ):
-    server_manager.start_server(id)
+    server_manager.start(id)
 
 
 @minecraft_server_router.post("/{id}/stop")
 async def stop_minecraft_server(id: str, server_manager: DockerServerManagerDependency):
-    server_manager.stop_server(id)
+    server_manager.stop(id)
