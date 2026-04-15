bfe6de87fcd7508db775abfc0c4707f9badbd8ca
Dimitar Dimitrov <105887060+Dimpex0@users.noreply.github.com>
2026-04-15 17:46:14 +0300

    feat: added custom Pydantic itzg image configuration (#11)
    * First working version of the configuration
    
    * added all MVP variables to the custom config
    
    * feat: add string docs to status method

--- services/docker_server_manager.py
+++ services/docker_server_manager.py
@@ -1,11 +1,19 @@
+import os
+import shutil
+
 import docker
+from docker.models.containers import Container
 
 from exceptions.client_api_exception import ClientAPIException
+from exceptions.docker_container_not_found_exception import DockerContainerNotFoundException
 from exceptions.docker_image_not_found_exception import DockerImageNotFoundException
 from exceptions.server_name_already_exists_exception import ServerNameAlreadyExistsException
+from schemas.minecraft_server_itzg_config import MinecraftServerITZGConfig
+from schemas.minecraft_server_status import MinecraftServerStatus, Status
 from server_manager import ServerManager
 
 DOCKER_MINECRAFT_IMAGE = "itzg/minecraft-server"
+WORLDS_DIR = os.environ.get("WORLDS_DIR", "..")
 
 # TODO: add logging
 class DockerServerManager(ServerManager):
@@ -13,23 +21,41 @@ class DockerServerManager(ServerManager):
         try:
             self.client = docker.from_env()
         except Exception as err:
+            print(err)
             raise ClientAPIException(str(err))
 
-    def create_server(self, server_name: str) -> str | None:
+    def create(self, config: MinecraftServerITZGConfig) -> str | None:
+        """Creates an itzg/minecraft-server container with the given configuration"""
         try:
-            if self.__server_exists(server_name):
-                raise ServerNameAlreadyExistsException(server_name)
+            if self.__exists(config.container_name):
+                raise ServerNameAlreadyExistsException(config.container_name)
 
             self.client.images.pull("itzg/minecraft-server", tag="latest")
 
+            host_path = os.path.abspath(f"{WORLDS_DIR}/data/{config.container_name}")
+            if not os.path.exists(host_path):
+                os.makedirs(host_path)
+            else:
+                shutil.rmtree(host_path)
+                os.makedirs(host_path)
+
             container = self.client.containers.create(
                 DOCKER_MINECRAFT_IMAGE,
-                name=f"{server_name}",
-                environment={"EULA": "TRUE", "MEMORY": f"{4}G"}, # TODO: memory must come from config param
+                name=f"{config.container_name}",
+                environment=config.to_docker_env(),
+                nano_cpus=int(config.cpu_cores * (10 ** 9)), # 1 core = 10 ^ 9
+                volumes={
+                    host_path: {
+                        "bind": "/data",
+                        "mode": "rw,Z"
+                    }
+                },
+                ports={
+                    '25565/tcp': 25565,  # Minecraft Game Port
+                    '25575/tcp': 25575  # RCON Port
+                },
                 detach=True,
-                nano_cpus=1 * (10 ** 9), # TODO: cpu must come from config param
             )
-            # TODO: mount volume for persistent worlds
 
         except docker.errors.ImageNotFound:
             raise DockerImageNotFoundException(DOCKER_MINECRAFT_IMAGE)
@@ -38,21 +64,81 @@ class DockerServerManager(ServerManager):
 
         return container.name
 
-    def start_server(self, server_id: str) -> bool:
+    def status(self, server_id: str) -> MinecraftServerStatus:
+        try:
+            container: Container = self.client.containers.get(server_id)
+
+            status = container.status
+            log = "No logs"
+
+            # If the minecraft instance was very recently created, log files are not yet generated
+            host_path = os.path.abspath(f"{WORLDS_DIR}/data/{container.name}/logs/latest.log")
+            if os.path.exists(host_path):
+                with open(host_path, "r") as log_file:
+                    log = log_file.read()
+
+            return MinecraftServerStatus(status=Status(value=status), log=log)
+
+        except docker.errors.NotFound:
+            raise DockerContainerNotFoundException(server_id)
+        except docker.errors.APIError as err:
+            raise ClientAPIException(err)
+
+    def start(self, server_id: str) -> bool:
         try:
             self.client.containers.get(server_id).start()
-        except docker.errors.APIError:
+        except docker.errors.NotFound:
+            return False
+        except docker.errors.APIError as err:
             return False
 
         return True
 
-    def stop_server(self, server_id: str) -> bool:
+    def stop(self, server_id: str) -> bool:
         try:
             self.client.containers.get(server_id).stop()
-        except docker.errors.APIError:
+        except docker.errors.NotFound:
+            return False
+        except docker.errors.APIError as err:
             return False
 
         return True
 
-    def __server_exists(self, server_name: str) -> bool:
-        return self.client.containers.list(all=True, filters={"name": server_name})
+    def __exists(self, server_name: str) -> bool:
+        try:
+            self.client.containers.get(server_name)
+            return True
+        except docker.errors.NotFound:
+            return False
+        except docker.errors.APIError as err:
+            raise ClientAPIException(str(err))
+
+
+# FOR TESTING
+
+# manager = DockerServerManager()
+
+# config: MinecraftServerITZGConfig = MinecraftServerITZGConfig(
+#     server_name="dimpex",
+#     memory=2,
+#     cpu_cores=1,
+#     type="VANILLA",
+#     eula=True,
+#     difficulty="hard",
+#     mode="creative",
+#     level="swqt",
+#     resource_pack="https://download.mc-packs.net/pack/59eb5f3c13e5a064dae879cce3e4c6aff6bf9b87.zip",
+#     resource_pack_sha1="59eb5f3c13e5a064dae879cce3e4c6aff6bf9b87",
+#     online_mode=True,
+#     enable_whitelist=True,
+#     whitelist=["Dimpex", "Dimitar45"],
+#     container_name="dimpex",
+#     rcon_password="123"
+# )
+#
+# manager.create(config)
+# manager.start(config.server_name)
+# # manager.stop(config.server_name)
+# status: MinecraftServerStatus = manager.status(config.server_name)
+# print(status.status.name)
+# print(status.log)
