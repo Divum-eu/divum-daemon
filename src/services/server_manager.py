8f477a8510caba872904513cb205fee56a861584
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-04-19 18:57:16 +0300

    fix: changed function definitions to async

--- src/services/server_manager.py
+++ src/services/server_manager.py
@@ -7,21 +7,23 @@ from schemas.minecraft_server_itzg_config import MinecraftServerITZGConfig
 class ServerManager(ABC):
 
     @abstractmethod
-    def create(self, config: MinecraftServerITZGConfig) -> str | None:
+    async def create(
+        self, config: MinecraftServerITZGConfig
+    ) -> str | None:
         """Creates a server and return the unique identifier for it"""
         pass
 
     @abstractmethod
-    def status(self, server_id: str) -> MinecraftServerStatus:
+    async def status(self, server_id: str) -> MinecraftServerStatus:
         """Returns the status of an existing instance and it's logs"""
         pass
 
     @abstractmethod
-    def start(self, server_id: str) -> bool:
+    async def start(self, server_id: str) -> bool:
         """Starts an existing server"""
         pass
 
     @abstractmethod
-    def stop(self, server_id: str) -> bool:
+    async def stop(self, server_id: str) -> bool:
         """Stops an existing server"""
         pass
