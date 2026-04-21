38b0210f88956d12d6247df411ef890c0fc21f6a
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-04-21 07:40:13 +0300

    fix: wrap docker sdk calls in asyncio threads

--- src/services/docker_server_manager.py
+++ src/services/docker_server_manager.py
@@ -1,37 +1,57 @@
+"""
+Contains the main service responsible for handling Minecraft server container instances.
+"""
+
+import asyncio
+
 import os
+
 import shutil
 
 import docker
 from docker.models.containers import Container
+from docker.errors import APIError, ImageNotFound, NotFound
+
+from constants.constants import MINECRAFT_SERVER_DOCKER_IMAGE
 
 from exceptions.client_api_exception import ClientAPIException
+from exceptions.docker_container_not_found_exception import (
+    DockerContainerNotFoundException,
+)
 from exceptions.docker_image_not_found_exception import DockerImageNotFoundException
-from exceptions.server_name_already_exists_exception import ServerNameAlreadyExistsException
+from exceptions.server_name_already_exists_exception import (
+    ServerNameAlreadyExistsException,
+)
 
 from services.server_manager import ServerManager
 
 from schemas.minecraft_server_itzg_config import MinecraftServerITZGConfig
 from schemas.minecraft_server_status import MinecraftServerStatus, Status
 
-DOCKER_MINECRAFT_IMAGE = "itzg/minecraft-server"
 WORLDS_DIR = os.environ.get("WORLDS_DIR", "..")
+
 
 # TODO: add logging
 class DockerServerManager(ServerManager):
+    """
+    The service responsible for handling Minecraft server container instances.
+    """
+
     def __init__(self):
         try:
-            self.client = docker.from_env()
+            self._client = docker.from_env()
         except Exception as err:
-            print(err)
-            raise ClientAPIException(str(err))
+            raise ClientAPIException("Couldn't start the Docker client.") from err
 
-    def create(self, config: MinecraftServerITZGConfig) -> str | None:
+    async def create(self, config: MinecraftServerITZGConfig) -> str | None:
         """Creates an itzg/minecraft-server container with the given configuration"""
         try:
             if self.__exists(config.container_name):
                 raise ServerNameAlreadyExistsException(config.container_name)
 
-            self.client.images.pull("itzg/minecraft-server", tag="latest")
+            await asyncio.to_thread(
+                self._client.images.pull, MINECRAFT_SERVER_DOCKER_IMAGE, tag="latest"
+            )
 
             host_path = os.path.abspath(f"{WORLDS_DIR}/data/{config.container_name}")
             if not os.path.exists(host_path):
@@ -40,79 +60,92 @@ class DockerServerManager(ServerManager):
                 shutil.rmtree(host_path)
                 os.makedirs(host_path)
 
-            container = self.client.containers.create(
-                DOCKER_MINECRAFT_IMAGE,
+            container: Container = await asyncio.to_thread(
+                self._client.containers.create,
+                MINECRAFT_SERVER_DOCKER_IMAGE,
                 name=f"{config.container_name}",
                 environment=config.to_docker_env(),
-                nano_cpus=int(config.cpu_cores * (10 ** 9)), # 1 core = 10 ^ 9
-                volumes={
-                    host_path: {
-                        "bind": "/data",
-                        "mode": "rw,Z"
-                    }
-                },
+                nano_cpus=int(config.cpu_cores * (10**9)),  # 1 core = 10 ^ 9
+                volumes={host_path: {"bind": "/data", "mode": "rw,Z"}},
                 ports={
-                    '25565/tcp': 25565,  # Minecraft Game Port
-                    '25575/tcp': 25575  # RCON Port
+                    "25565/tcp": 25565,  # Minecraft Game Port
+                    "25575/tcp": 25575,  # RCON Port
                 },
                 detach=True,
             )
 
-        except docker.errors.ImageNotFound:
-            raise DockerImageNotFoundException(DOCKER_MINECRAFT_IMAGE)
-        except docker.errors.APIError as err:
-            raise ClientAPIException(err)
+        except ImageNotFound as ex:
+            raise DockerImageNotFoundException(MINECRAFT_SERVER_DOCKER_IMAGE) from ex
+        except APIError as err:
+            raise ClientAPIException(
+                f"An error occurred while trying to create a container from image '{MINECRAFT_SERVER_DOCKER_IMAGE}'."
+            ) from err
 
         return container.name
 
-    def status(self, server_id: str) -> MinecraftServerStatus:
+    async def status(self, server_id: str) -> MinecraftServerStatus:
         try:
-            container: Container = self.client.containers.get(server_id)
+            container: Container = await asyncio.to_thread(
+                self._client.containers.get, server_id
+            )
 
             status = container.status
             log = "No logs"
 
             # If the minecraft instance was very recently created, log files are not yet generated
-            host_path = os.path.abspath(f"{WORLDS_DIR}/data/{container.name}/logs/latest.log")
+            host_path = os.path.abspath(
+                f"{WORLDS_DIR}/data/{container.name}/logs/latest.log"
+            )
             if os.path.exists(host_path):
                 with open(host_path, "r") as log_file:
                     log = log_file.read()
 
             return MinecraftServerStatus(status=Status(value=status), log=log)
 
-        except docker.errors.NotFound:
-            raise DockerContainerNotFoundException(server_id)
-        except docker.errors.APIError as err:
-            raise ClientAPIException(err)
+        except NotFound as ex:
+            raise DockerContainerNotFoundException(server_id) from ex
+        except APIError as err:
+            raise ClientAPIException(
+                f"An error occurred while trying get the status of container '{server_id}'."
+            ) from err
 
-    def start(self, server_id: str) -> bool:
+    async def start(self, server_id: str) -> bool:
         try:
-            self.client.containers.get(server_id).start()
-        except docker.errors.NotFound:
-            return False
-        except docker.errors.APIError as err:
+            container: Container = await asyncio.to_thread(
+                self._client.containers.get, server_id
+            )
+
+            await asyncio.to_thread(container.start)
+
+        except (NotFound, APIError):
             return False
 
         return True
 
-    def stop(self, server_id: str) -> bool:
+    async def stop(self, server_id: str) -> bool:
         try:
-            self.client.containers.get(server_id).stop()
-        except docker.errors.NotFound:
-            return False
-        except docker.errors.APIError as err:
+            container: Container = await asyncio.to_thread(
+                self._client.containers.get, server_id
+            )
+
+            await asyncio.to_thread(container.stop)
+
+        except (NotFound, APIError):
             return False
 
         return True
 
-    def __exists(self, server_name: str) -> bool:
+    async def __exists(self, server_name: str) -> bool:
         try:
-            self.client.containers.get(server_name)
+            await asyncio.to_thread(self._client.containers.get, server_name)
+
             return True
-        except docker.errors.NotFound:
+        except NotFound:
             return False
-        except docker.errors.APIError as err:
-            raise ClientAPIException(str(err))
+        except APIError as err:
+            raise ClientAPIException(
+                f"An error occurred trying to find container '{server_name}'."
+            ) from err
 
 
 # FOR TESTING
